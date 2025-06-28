ea69fa01c1618a3ea2968c19fe92620107e8d577
Umakant Yadav <umakantchandra180@gmail.com>
2025-06-28 03:02:02 -0700

    third commit

---
+++ React JS KG/Projects/calculator-version-2/calculator/src/components/buttonsContainer.jsx
@@ -0,0 +1,41 @@
+import styles from "./buttonsContainer.module.css";
+const ButtonsContainer = ({ onButtonClick }) => {
+  const ButtonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <>
+      <div className={styles.buttonContainer}>
+        {/* {ButtonNames.map((buttonName) => (
+          <button className={styles.button}>C</button>
+        ))} */}
+
+        {ButtonNames.map((buttonName) => (
+          <button
+            className={styles.button}
+            onClick={() => onButtonClick(buttonName)}
+          >
+            {buttonName}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+export default ButtonsContainer;
